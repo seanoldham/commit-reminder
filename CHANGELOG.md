b0444421a803ecfbe0fe4b76fc7f496956ccc358
Sean Oldham <seanosaur@me.com>
2016-05-11 17:03:26 -0500

    late initial commit

---
+++ CHANGELOG.md
@@ -0,0 +1,2 @@
+## 0.1.0 - Initial Release
+* On toggle, starts counting keystrokes until it hits 1000, then reminds user to `git commit`.
---
+++ spec/commit-reminder-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import CommitReminderView from '../lib/commit-reminder-view';
+
+describe('CommitReminderView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
